b1d07362e57c77ce6208c1ab9cf4bd4d7f512899
xiong.bob@gmail.com <xiong.bob@gmail.com>
2019-05-24 12:36:53 -0400

    whatever-commit-exe203

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+public class Employee {
+    private String fname;
+    private String lname;
+    private int SSN;
+    private String DOB;
+
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public int getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(int SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+}
